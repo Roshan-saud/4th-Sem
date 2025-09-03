8059d6b670ffee9749464a298870888c2f4c284f
Roshan-saud <saudroshan2739@gmail.com>
2025-09-03 12:11:49 +0545

    feat: Update cpp code of TOC

---
+++ TOC/lab10.cpp
@@ -0,0 +1,28 @@
+// LAB10. PDA for L={a^n b^n : n>=1}
+
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+// simulate PDA
+bool f(string w){
+    stack<char> st;
+    for(char x:w){
+        if(x=='a') st.push('a');        // push 'a'
+        else if(x=='b'){                // pop 'a'
+            if(st.empty()) return false;
+            st.pop();
+        }
+        else return false;              // invalid char
+    }
+    return st.empty();                  // accept if stack empty
+}
+
+int main(){
+    string w;
+    cout<<"Enter: ";
+    cin>>w;
+    if(f(w)) cout<<"Accepted\n";
+    else cout<<"Rejected\n";
+}
